590153dd7c575b83b1794333b864553e8b176530
Josh Calahan <jcalahan@gmail.com>
2010-11-04 20:50:33 -0500

    display_mpi prep

--- hw6/display_mpi.cpp
+++ hw6/display_mpi.cpp
@@ -1,6 +1,6 @@
 // Graphic Transformation/Scaling Program for 256 color
 // 640 x 580 window bit map file
-// Josh Calahan, Novemver 2010, MPI Version, based on
+// Josh Calahan, November 2010, MPI Version, based on
 // B. Earl Wells, October 2010, University of Alabama in Huntsville
 // serial version more condusive to MPI Scatter and Gather of display
 // arrays. Uses dynamic declarations of display arrays similar
@@ -58,10 +58,14 @@ void get_params(int *delta_X,int *delta_Y,int *X,int *Y,
 }
 
 int main( int argc, char *argv[])  {
-   int byte_cnt,num_rows, num_cols;
+
    char file[80];
-   int i,j;
-   int delta_X,delta_Y,delta_X_,delta_Y_,X,Y,X_,Y_,x,y,x_,y_;
+
+   int num_rows, num_cols,
+       delta_X, delta_Y,
+       delta_X_, delta_Y_,
+       X, Y,
+       X_, Y_;
 
    if (argc != 2) {
       cout << "Usage: display [file name (no extension)]" << endl;
@@ -69,9 +73,9 @@ int main( int argc, char *argv[])  {
    }
 
    // read a bit map file such as shut.bmp to get initial image 
-   strcpy(file,argv[1]);
-   strcat(file,".bmp");
-   read_256_bmp(file, &num_rows,&num_cols,&display_in);
+   strcpy(file, argv[1]);
+   strcat(file, ".bmp");
+   read_256_bmp(file, &num_rows, &num_cols, &display_in);
 
    // declare display out region
    display_out = new (nothrow) unsigned char [(num_cols+3)*num_rows];
@@ -79,21 +83,26 @@ int main( int argc, char *argv[])  {
      cout <<"ERROR:  Insufficient Memory" << endl;
      exit(1);
    }
-   cout << "num_rows=" << num_rows << " num_cols=" << num_cols << endl;
-   // clear out output display area
-   for (i=0;i<num_rows;i++) for (j=0;j<num_cols;j++) Display_out(i,j)=255;
 
-   get_params(&delta_X,&delta_Y,&X,&Y,&delta_X_,&delta_Y_,&X_,&Y_);
-   for (x=X;x<X+delta_X;x++) {
-      for (y=Y;y<Y+delta_Y;y++) {
-         x_= ((float) delta_X_/(float) delta_X)*(float) (x-X) + X_; 
-         y_= ((float) delta_Y_/(float) delta_Y)*(float) (y-Y) + Y_;
+   cout << "num_rows=" << num_rows << " num_cols=" << num_cols << endl;
+
+   // clear out output display area
+   for (int i=0; i<num_rows; ++i)
+      for (int j=0; j<num_cols; ++j)
+         Display_out(i,j)=255;
+
+   get_params(&delta_X,&delta_Y, &X,&Y, &delta_X_,&delta_Y_, &X_,&Y_);
+
+   for (int x=X; x<X+delta_X; ++x) {
+      for (int y=Y; y<Y+delta_Y; ++y) {
+         int x_ = (float(delta_X_) / float(delta_X)) * float(x-X) + X_; 
+         int y_ = (float(delta_Y_) / float(delta_Y)) * float(y-Y) + Y_;
          if (x_<num_rows && y_<num_cols)  
             Display_out(x_,y_) = Display_in(x,y);
       }
    } 
 
-   strcpy(file,argv[1]);
-   strcat(file,"_serial.bmp");
-   write_256_bmp(file, num_rows,num_cols,display_out);
+   strcpy(file, argv[1]);
+   strcat(file, "_serial.bmp");
+   write_256_bmp(file, num_rows,num_cols, display_out);
 }
