c287575ce32f7735d276d7d1f3d39d161abe021f
Josh Calahan <jcalahan@gmail.com>
2010-11-06 21:12:02 -0500

    Refactored test_display.sh

--- hw6/test_display.sh
+++ hw6/test_display.sh
@@ -1,97 +1,32 @@
 #!/bin/bash
 
-./display_serial.exe images/red_green_256 << EOF
+cat > display.in << EOF
 360
 360
-0
-0
+20
+20
 200
 200
 50
 50
 EOF
 
-mpirun -np 8 ./display_mpi.exe images/red_green_256 << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
-
-./display_pthread.exe images/red_green_256 8 << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
-
-./display_omp.exe images/red_green_256 8 << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
+./display_serial.exe images/red_green_256 < display.in > /dev/null
+mpirun -np 8 ./display_mpi.exe images/red_green_256 < display.in > /dev/null
+./display_pthread.exe images/red_green_256 8 < display.in > /dev/null
+./display_omp.exe images/red_green_256 8 < display.in > /dev/null
 
 cmp images/red_green_256_{serial,mpi}.bmp
 cmp images/red_green_256_{serial,pthread}.bmp
 cmp images/red_green_256_{serial,omp}.bmp
 
-./display_serial.exe images/shut << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
-
-mpirun -np 8 ./display_mpi.exe images/shut << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
-
-./display_pthread.exe images/shut 8 << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
-
-./display_omp.exe images/shut 8 << EOF
-360
-360
-0
-0
-200
-200
-50
-50
-EOF
+./display_serial.exe images/shut < display.in > /dev/null
+mpirun -np 8 ./display_mpi.exe images/shut < display.in > /dev/null
+./display_pthread.exe images/shut 8 < display.in > /dev/null
+./display_omp.exe images/shut 8 < display.in > /dev/null
 
 cmp images/shut_{serial,mpi}.bmp
 cmp images/shut_{serial,pthread}.bmp
 cmp images/shut_{serial,omp}.bmp
+
+unlink display.in
